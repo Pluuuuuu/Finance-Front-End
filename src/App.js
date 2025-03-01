3d1230ba5733d5962d879e5527d3bd4403a61e66
Michella <michella.reaidy@gmail.com>
2025-03-01 20:29:19 +0200

    updated expense form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import ExpenseList from "./Pages/ExpenseList";
 import ExpenseForm from "./Pages/ExpenseForm";
+import Home from "./Pages/Home";
+import Income from "./Pages/Income";
+import Analysis from "./Pages/Analysis";
 
 function App() {
     const [updateTrigger, setUpdateTrigger] = useState(false);
 
     return (
-        <div>
-            <h1>Finance Tracker</h1>
-            <ExpenseForm onExpenseAdded={() => setUpdateTrigger(!updateTrigger)} />
-            <ExpenseList key={updateTrigger} />
-        </div>
+        <Router>
+            <Navbar />
+            <div>
+                <h1>Finance Tracker</h1>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/income" element={<Income />} />
+                    <Route path="/expense" element={
+                        <>
+                            <ExpenseForm onExpenseAdded={() => setUpdateTrigger(!updateTrigger)} />
+                            <ExpenseList key={updateTrigger} />
+                        </>
+                    } />
+                    <Route path="/analysis" element={<Analysis />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
